9c2d5ff6d91ade71a87294613db582a1c043df65
A2GMer <52153162+A2GMer@users.noreply.github.com>
2023-01-21 20:44:57 +0900

    データを取得

--- MyCoreData/ContentView.swift
+++ MyCoreData/ContentView.swift
@@ -13,6 +13,13 @@ struct ContentView: View {
     // Contextが格納されている
     @Environment(\.managedObjectContext) var viewContext
     
+    // データを要求
+    // sortDescriptors: 取得したデータの並びを指定。[]指定なし
+    // 取得したデータは、humansに格納
+    // FetchedResults<Entity名>
+    @FetchRequest(sortDescriptors: [])
+    var humans: FetchedResults<Human>
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
