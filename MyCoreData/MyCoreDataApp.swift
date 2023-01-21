c824dfa4dea11bc4f480b21e4bff5f5befeb699e
A2GMer <52153162+A2GMer@users.noreply.github.com>
2023-01-21 20:21:43 +0900

    Initial Commit

---
+++ MyCoreData/MyCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCoreDataApp.swift
+//  MyCoreData
+//
+//  Created by 田中大翔 on 2023/01/21.
+//
+
+import SwiftUI
+
+@main
+struct MyCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
